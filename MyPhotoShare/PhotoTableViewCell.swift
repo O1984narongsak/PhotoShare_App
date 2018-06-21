bff4bedb95ec52d517804931428ed33229696592
Narongsak Launwijit <censpi@gmail.com>
2018-06-21 12:33:39 +0700

    first commit

---
+++ MyPhotoShare/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  MyPhotoShare
+//
+//  Created by Narongsak Luanwijit on 20/6/2561 BE.
+//  Copyright © 2561 Narongsak Luanwijit. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
